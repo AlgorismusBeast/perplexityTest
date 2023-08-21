3550d8e14c5414789c045b785991e7b5f7599d43
AlgorismusBeast <hunter_avery@outlook.com>
2023-08-21 13:56:53 -0400

    slight button change

--- perplexityTest.js
+++ perplexityTest.js
@@ -51,12 +51,8 @@ async function perplexityTest() {
     // await page.click(".ml-md > button"), { delay: 10000 };
 
     // NEW BUTTON ATTEMPT
-    await page.waitForSelector(".ml-md button[type='button']", { timeout: 10000 });
-    await page.click(".ml-md button[type='button']", { delay: 10000 });
-
-    await page.waitForSelector(".max-w-sm input"), { delay: 10000 };
-    await page.click(".max-w-sm input"), { delay: 10000 };
-    await page.type(".max-w-sm input", 'a@a.com', { delay: 10000 });
+    await page.waitForSelector(".ml-md.mt-md button[type='button']", { timeout: 10000 });
+    await page.click(".ml-md.mt-md button[type='button']", { delay: 10000 });
 
     await page.setRequestInterception(true);
     page.on('request', async request => {
